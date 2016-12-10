d57196d7f153f6cd04ee3adc784ede1c34f34575
TonnamP <mytjn14@gmail>
2016-12-10 16:59:09 +0700

    bot-face

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.post('/webhook/', function (req, res) {
         sendGenericMessage(sender)
         continue
       }
-      sendTextMessage(sender, 'What ?' + text.substring(0, 200))
+      sendTextMessage(sender, ' ' + text.substring(0, 200))
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
-      sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
+      sendTextMessage(sender, ' ' + text.substring(0, 200), token)
       continue
     }
   }
